561d746515120a7f6d1e7e45c8da8dd7bfebda75
josehenriquepg <henricopg11@gmail.com>
2024-06-26 01:55:00 -0300

    feat: dark mode/light mode

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function toggleMenu() {
   const menu = document.querySelector(".links-menu");
   const icon = document.querySelector(".hamburger-icon");
+
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const chk = document.getElementById("chk")
+
+chk.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+})
